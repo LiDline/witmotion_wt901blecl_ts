3d48ee85feacc21b910e90d7ecc141af9fe03a52
LiDline <artyom.zhevnerenko@yandex.ru>
2023-04-19 17:25:22 +0300

    Добавлено считывание по BLE

--- src/functions/ConnectToBluetooth.ts
+++ src/functions/ConnectToBluetooth.ts
@@ -1,5 +1,5 @@
 import { DeviceInterface } from "./ConnectToUsb";
-import { SensorData } from "./ExtractDataFromRaw";
+import { SensorData, extractDataFromRaw } from "./ExtractDataFromRaw";
 
 export class UsingBluetooth implements DeviceInterface {
   private serviceUUID = "0000ffe5-0000-1000-8000-00805f9a34fb";
@@ -9,18 +9,28 @@ export class UsingBluetooth implements DeviceInterface {
 
   // Подключаемся к USB
   Connect = async (onDataReceived?: (data: SensorData) => void) => {
+    // Ищем датчик по uuid его сервиса (он единственный) и подключаемся
     const device = await navigator.bluetooth.requestDevice({
       filters: [{ services: [this.serviceUUID] }],
     });
     this.server = await device.gatt?.connect();
 
+    // Подключаемся к сервису и нужной характеристике (сейчас считывание)
     const service = await this.server?.getPrimaryService(this.serviceUUID);
     const characteristic = await service?.getCharacteristic(this.readUUID);
 
-    // Получим одно уведомление с датчика
-    const value = await characteristic?.readValue();
-    console.log(value);
+    // Создаём прослушиваетель событий, в котором будем преобразовывать и отправлять получаемые данные
+    characteristic?.addEventListener("characteristicvaluechanged", (event) => {
+      const value = (event.target as BluetoothRemoteGATTCharacteristic).value;
+      if (value !== undefined) {
+        const byteData = new Uint8Array(value.buffer);
 
+        onDataReceived && onDataReceived(extractDataFromRaw(byteData));
+      }
+    });
+
+    // Запускаем прослушиватель
+    await characteristic?.startNotifications();
   };
 
   // Отключение от устройства
